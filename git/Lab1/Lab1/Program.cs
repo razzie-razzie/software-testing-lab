8ac138b8f04b0e29a1b5ca42c5dac0765796868e
marinka.mms@gmail.com <marinka.mms@gmail.com>
2018-09-14 22:37:06 +0300

    first commit

--- git/Lab1/Lab1/Program.cs
+++ git/Lab1/Lab1/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Lab1
 {
+    //commit one
     class Program
     {
         static List<string> Students = new List<string>() { "Малинчик Марина", "Якубейко Юлия",
